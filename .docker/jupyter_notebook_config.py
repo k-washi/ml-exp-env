98c723c8d0b5ee62a6d4a3f5f830361ce777f6b9
Kai Washizaki <washizaki@fusic.co.jp>
2021-04-09 18:29:39 +0900

    add docker env

---
+++ .docker/jupyter_notebook_config.py
@@ -0,0 +1,16 @@
+import os
+from IPython.lib import passwd
+
+c = c
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = int(os.getenv('PORT', 8888))
+c.NotebookApp.open_browser = False
+
+if 'PASSWORD' in os.environ:
+  password = os.environ['PASSWORD']
+  if password:
+    c.NotebookApp.password = passwd(password)
+  else:
+    c.NotebookApp.password = ''
+    c.NotebookApp.token = ''
+  del os.environ['PASSWORD']
