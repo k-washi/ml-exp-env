7ea2d7f09855409c6874e96ce02a3c0031f221bd
k-washi <kai.washizaki@gmail.com>
2022-07-28 16:32:48 +0900

    saved

--- src/util/logger.py
+++ src/util/logger.py
@@ -1,61 +1,66 @@
 import logging
-from logging import Logger
+import logging.handlers # handlersを使用するため呼び出し必須
 from pathlib import Path
 from typing import Dict, Optional
 
-format = "%(asctime)s (%(module)s:%(lineno)d) %(levelname)s: %(message)s"
+_log_initialized: Dict[str, logging.Logger] = {}
 
-_initialized: Dict[str, bool] = {}
-
-
-def getLogger(
-    verbose: int = 0,
+def get_logger(
+    debug: bool = False,
     filename: Optional[str] = None,
-    name: str = "",
+    name: str = "main",
     add_stream_handler: bool = True,
-) -> Logger:
-    """Get a logger instance.
+    file_max_bytes=100000, 
+    file_backup_count=1
+) -> logging.Logger:
+    """loggerを取得
+    関数を読み込む前に実行
     Args:
-        verbose (int): Verbosity level. Can be a number from -1 (low) to 10 (high).
-        filename (str, optional): Name of the file to log to.
-        name (str, optional): Name of the logger.
-        add_stream_handler (bool, optional): Add a stream handler to the logger.
+        debug (bool): デバッグモードにするか?, Falseの場合、INFO
+        filename (str, optional): ログのファイル出力先
+        name (str, optional): ログの名前
+        add_stream_handler (bool, optional): ストリーム出力
     Returns:
         logging.Logger: Logger instance.
-    Examples:
-        >>> from ttsdatacreate.logger import getLogger
-        >>> logger = getLogger(verbose=10)
-        >>> logger.info("This is a test")
-        2021-07-24 10:20:52,924 (ttslearn:23) INFO: This is a test
-        >>> logger.debug("This is a debug message")
-        2021-07-24 10:20:52,924 (ttslearn:23) DEBUG: This is a debug message
-        >>> logger.warning("This is a warning message")
-        2021-07-24 10:20:52,924 (ttslearn:23) WARNING: This is a warning message
     """
-    global _initialized
-    logger = logging.getLogger(name)
-    if verbose >= 10:
-        logger.setLevel(logging.DEBUG)
-    elif verbose > 0:
-        logger.setLevel(logging.INFO)
-    else:
-        logger.setLevel(logging.WARN)
-
-    if _initialized.get(name, False):
+    global _log_initialized
+    logger = _log_initialized.get(name, None)
+    if logger is not None:
         return logger
+    
+    format = '%(levelname)-8s: %(asctime)s | %(filename)-12s - %(funcName)-12s : %(lineno)-4s -- %(message)s'
+    logger = logging.getLogger(name)
+    
+    #ログレベルの設定
+    if debug:
+        logger.setLevel(logging.DEBUG)
     else:
-        _initialized[name] = True
+        logger.setLevel(logging.INFO)
 
     if add_stream_handler:
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(logging.Formatter(format))
         logger.addHandler(stream_handler)
-
+    
+    # ログファイルに関する設定
     if filename is not None:
         Path(filename).parent.mkdir(parents=True, exist_ok=True)
-        file_handler = logging.FileHandler(filename=filename)
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(logging.Formatter(format))
+        file_handler = logging.handlers.RotatingFileHandler(
+            filename, 
+            maxBytes=file_max_bytes, 
+            backupCount=file_backup_count,
+            mode="a+", # 開くか、新しいテキストファイルを作って最後から更新
+            encoding="utf-8"
+            )
+        file_handler.setFormatter(logging.Formatter(
+            format, datefmt='%Y-%m-%d %H:%M:%S'
+            ))
         logger.addHandler(file_handler)
-
+        
+    _log_initialized[name] = logger
     return logger
+
+if __name__ == "__main__":
+    logger = get_logger(debug=False)
+    logger.info("info message")
+    logger.debug("debug message")
